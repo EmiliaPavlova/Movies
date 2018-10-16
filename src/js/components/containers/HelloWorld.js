417be7dbf5e511cd060f30f6bc772caba915e31b
Emilia Pavlova <emiliya_pavlova@epam.com>
2018-10-16 12:32:00 +0300

    initial commit

---
+++ src/js/components/containers/HelloWorld.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class HellWorld extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: 'Hello World!'
+    };
+  }
+  render() {
+    return (
+      <div>{this.state.title}</div>
+    );
+  }
+}
+export default HellWorld;
+
+const wrapper = document.getElementById('index');
+wrapper ? ReactDOM.render(<HellWorld />, wrapper) : false;
+
