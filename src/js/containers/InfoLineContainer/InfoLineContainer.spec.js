721009af55f3cf663ad7a9e84695baa546176380
Emilia Pavlova <emilia.pavlova@gmail.com>
2018-10-27 00:46:35 +0300

    add some tests

--- src/js/containers/InfoLineContainer/InfoLineContainer.spec.js
+++ src/js/containers/InfoLineContainer/InfoLineContainer.spec.js
@@ -3,13 +3,31 @@ import { mount } from 'enzyme';
 import InfoLineContainer from './InfoLineContainer';
 import './InfoLineContainer.css';
 
+const componentProps = (overrides = {}) => ({
+  page: 'home',
+  moviesFound: 2,
+  sortBy: 'release_date',
+  ...overrides
+});
+
 const mountInfoLineContainer = props => mount(<InfoLineContainer {...props} />);
 
 describe('InfoLineContainer', () => {
   describe('Snapshot', () => {
-    it('matches the snapshot', () => {
-      const wrapper = mountInfoLineContainer();
-      expect(wrapper).toMatchSnapshot();
+    it('matches the snapshot for Home page', () => {
+      const wrapper = mountInfoLineContainer(componentProps());
+      expect(wrapper).toMatchSnapshot('Home page');
+    });
+    it('matches the snapshot for Details page', () => {
+      const wrapper = mountInfoLineContainer(componentProps({page: 'details'}));
+      expect(wrapper).toMatchSnapshot('Details page');
     })
   });
+  describe('Home page', () => {
+    it('Should return InfoLine', () => {
+      const props = componentProps();
+      const wrapper = mountInfoLineContainer(props);
+      console.log(wrapper.debug());
+    })
+  })
 });
--- src/js/containers/SearchContainer/SearchContainer.spec.js
+++ src/js/containers/SearchContainer/SearchContainer.spec.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import SearchContainer from './SearchContainer';
 import './SearchContainer.css';
+
+const componentProps = (overrides = {}) => ({
+  onClick: jest.fn(),
+  ...overrides
+});
 
 const mountSearchContainer = props => mount(<SearchContainer {...props} />);
 
@@ -12,4 +17,14 @@ describe('SearchContainer', () => {
       expect(wrapper).toMatchSnapshot();
     })
   });
+  describe('Functions', () => {
+    it('calls onClick on click event', () => {
+      const props = componentProps();
+      const wrapper = mountSearchContainer(props);
+      const button = wrapper.find('button').at(0);
+      button.simulate('click');
+      expect(props.onClick).toHaveBeenCalled();
+      expect(props.onClick).toHaveBeenCalledTimes(1);
+    })
+  })
 });
